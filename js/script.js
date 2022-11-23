1d32ef1ccf395e2cb4f92638c102bdb16c5a35f0
Nicholas Bonner <nicholaslbonner@yahoo.com>
2022-11-23 12:19:47 -0800

    added comments to html fixed some styles in css and added some variables in js file

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
-
+const cityInputEl = document.querySelector('.city-input');
+const searchBtnEl = document.querySelector('.seacrh-btn');
+const currentCityInfoEl = document.querySelector('.current-weather-info');
+const cardsEl = document.querySelectorAll('.cards');
   
