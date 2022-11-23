1485de2d9124d8618858b961bd39a48cc3bd5070
Nicholas Bonner <nicholaslbonner@yahoo.com>
2022-11-23 14:55:50 -0800

    created all variables in js file and gave ids to some elements in html file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const cityInputEl = document.querySelector('.city-input');
 const searchBtnEl = document.querySelector('.seacrh-btn');
-const currentCityInfoEl = document.querySelector('.current-weather-info');
-const cardsEl = document.querySelectorAll('.cards');
-  
+const searchHistoryCon = document.querySelector('.search-history');
+// const currentCityInfoEl = document.querySelector('.current-weather-info');
+const currentCityTitleEl = document.querySelector('#current-city');
+const currentDayEl = document.querySelector('#current-day');
+const currentTempEl = document.querySelector('#current-temp');
+const currentWindEl = document.querySelector('#current-wind');
+const currentHumidityEl = document.querySelector('#current-himidity');
+
