e00e58915564287f0d5cffb06cc9025bd0e9ac80
Nicholas Bonner <nicholaslbonner@yahoo.com>
2022-11-23 17:59:19 -0800

    made first function in js file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
+// 
 const cityInputEl = document.querySelector('.city-input');
 const searchBtnEl = document.querySelector('.seacrh-btn');
 const searchHistoryCon = document.querySelector('.search-history');
 // const currentCityInfoEl = document.querySelector('.current-weather-info');
-const currentCityTitleEl = document.querySelector('#current-city');
+const currentCityEl = document.querySelector('#current-city');
 const currentDayEl = document.querySelector('#current-day');
 const currentTempEl = document.querySelector('#current-temp');
 const currentWindEl = document.querySelector('#current-wind');
 const currentHumidityEl = document.querySelector('#current-himidity');
+
+let formSubmitHander = function (event) {
+    event.preventDefault();
+
+    let cityInput = cityInputEl.value.trim();
+
+    if (cityInput) {
+        cityResults(cityInput);
+
+        currentCityEl.value = '';
+    } else {
+        currentCityEl.textContent = '';
+        alert ('Please enter a City');
+    }
+
+};
 
